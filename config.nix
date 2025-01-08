d1fbc768a3de85a97219f52bb86379fcdb320243
Tobias Pahl <tobias@pahlx.de>
2025-01-08 08:46:47 +0100

    cleanup

--- config.nix
+++ config.nix
@@ -5,10 +5,6 @@
 }: {
   imports = [
     ./keymaps.nix
-    ./plugins/language.nix
-    ./plugins/small_plugins.nix
-    ./plugins/cmp.nix
-    ./plugins/telescope.nix
   ];
   config.vim = {
     extraPlugins = with pkgs.vimPlugins; {
@@ -74,6 +70,13 @@
       cheatsheet.enable = true;
     };
 
+    statusline.lualine = {
+      enable = true;
+      theme = "catppuccin";
+    };
+    comments.comment-nvim.enable = true;
+    autopairs.nvim-autopairs.enable = true;
+
     spellcheck = {
       enable = false;
       languages = ["en" "de"];
@@ -112,6 +115,98 @@
           };
         };
       };
+    };
+
+    telescope = {
+      enable = true;
+      mappings = {
+        findFiles = "<leader>ff";
+        liveGrep = "<leader>fg";
+        buffers = "<leader>fb";
+        lspDocumentSymbols = "<leader>fs";
+      };
+    };
+
+    autocomplete.nvim-cmp = {
+      enable = true;
+      mappings = {
+        complete = "<C-Space";
+        confirm = "<Tab>";
+        next = "<C-n>";
+        previous = "<C-p>";
+        scrollDocsUp = "<C-b>";
+        scrollDocsDown = "<C-f>";
+      };
+      sources = {
+        buffer = "[Buffer]";
+        path = "[Path]";
+        rg = "[rg]";
+      };
+      setupOpts = {
+        experimental = {
+          ghost_text = true;
+        };
+      };
+    };
+
+    lsp = {
+      enable = true;
+      formatOnSave = true;
+      # lightbulb.enable = true;
+      # lspsaga.enable = true;
+      trouble.enable = true;
+      lspSignature.enable = true;
+      otter-nvim.enable = true;
+      lsplines.enable = true;
+      nvim-docs-view.enable = true;
+      mappings = {
+        codeAction = "<leader>ca";
+        goToDeclaration = "gD";
+        goToDefinition = "gT";
+        hover = "K";
+        nextDiagnostic = "[d";
+        previousDiagnostic = "]d";
+        renameSymbol = "<leader>cr";
+      };
+    };
+    debugger = {
+      nvim-dap = {
+        enable = true;
+        ui.enable = true;
+      };
+    };
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
+      enableFormat = true;
+      enableExtraDiagnostics = true;
+
+      nix = {
+        enable = true;
+        format = {
+          enable = true;
+          package = pkgs.alejandra;
+        };
+      };
+      rust = {
+        enable = true;
+        format = {
+          enable = true;
+          package = pkgs.rustfmt;
+        };
+      };
+      zig.enable = true;
+      clang.enable = true;
+      markdown.enable = true;
+    };
+    treesitter = {
+      enable = true;
+      context = {
+        enable = true;
+      };
+    };
+    snippets.luasnip = {
+      enable = true;
     };
   };
 }
